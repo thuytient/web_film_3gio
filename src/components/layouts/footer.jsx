b0806f7f8bd4fa7a998206ba201040b80e2d0e96
quangdao-can <quangdien.canada@gmail.com>
2025-08-28 23:03:32 -0600

    Update footer.jsx

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -7,11 +7,11 @@ function AppFooter() {
   }, []);
   return (
     <div className="footer__custom">
-		<div className="col-md-1 d-flex align-items-center">
+		
+      <footer className="mt-4 d-flex flex-wrap justify-content-between align-items-center p-3 border-top">
+	  <div className="col-md-0 d-flex align-items-center">
 	  <a>Tất cả nội dung của trang web này đều được tìm kiếm và thu thập ở các trang web phát video trực tuyến chính thống trên Internet, cũng như không cung cấp phát trực tuyến chính hãng.</a>
 	  </div>
-      <footer className="mt-4 d-flex flex-wrap justify-content-between align-items-center p-3 border-top">
-	  
         <div className="col-md-4 d-flex align-items-center">
           <a
             href="/"
@@ -38,5 +38,6 @@ function AppFooter() {
 }
 
 export default AppFooter;
+
 
 
