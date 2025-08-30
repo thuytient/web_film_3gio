529676a19a8accfdbf99af0d8cd6800621176b2c
quangdao-can <quangdien.canada@gmail.com>
2025-08-30 00:54:19 -0600

    Update MoviePlayer.jsx

--- src/app/xem-phim/[slug]/[tap]/components/MoviePlayer.jsx
+++ src/app/xem-phim/[slug]/[tap]/components/MoviePlayer.jsx
@@ -27,7 +27,7 @@ const VideoPlayer = ({ episode }) => {
       <iframe
         className="video-iframe rounded"
         title={episode.filename || "Video"}
-        src=url={`https://play.vnn69.com?url=${episode.link_m3u8}`} 
+        src={`https://play.vnn69.com?url=${episode.link_m3u8}`} 
         width="100%"
         height="500px"
         frameBorder="0"
@@ -155,5 +155,6 @@ const MoviePlayer = ({ initialData, params }) => {
 };
 
 export default MoviePlayer;
+
 
 
